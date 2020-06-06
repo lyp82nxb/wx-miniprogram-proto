6b41608f7bbc930dea2fd540a2f8d9f5ade8c4b1
fxdcode <fanxiaodong0123@qq.com>
2020-06-06 22:05:45 +0800

    fxd

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ App({
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 执行一次
    */
   onLaunch: function () {
 
@@ -10,9 +11,21 @@ App({
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   * 执行多次
    */
   onShow: function (options) {
-
+    //  1. 判断小程序的进入场景
+    console.log(options);
+    // 2. 获取用户信息 将用户信息传递给服务器
+    wx.getUserInfo({
+      complete: (res) => {
+        console.log(res);
+      },
+      success(res){
+        console.log(res);
+      }
+    })
+    // 2.2 获取用户信息 通过按钮实现
   },
 
   /**
